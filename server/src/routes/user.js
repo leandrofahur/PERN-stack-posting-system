794f3b4107a0d68aa0d9b346ddee2b4a6508f139
Leandro <fahurleandro@gmail.com>
2021-05-30 18:26:09 -0700

    login and sign up

--- server/src/routes/user.js
+++ server/src/routes/user.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const User = require("../models/User");
+const config = require("config");
+require("dotenv").config({ path: __dirname + "/../../.env" });
 
 /*
  * @route:  /user
@@ -13,7 +16,7 @@ router.post("/", async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    const user = await User.findOne({
+    let user = await User.findOne({
       where: {
         email: email,
       },
@@ -28,21 +31,96 @@ router.post("/", async (req, res) => {
     // ---
 
     // encryption:
-
-    console.log(password);
     const salt = await bcrypt.genSalt(10);
     const encryptedPassword = await bcrypt.hash(password, salt);
-    // ---
 
-    await User.create({
+    user = {
       username,
       email,
       password: encryptedPassword,
+    };
+
+    await User.create(user);
+
+    user = await User.findOne({
+      where: {
+        email: email,
+      },
     });
 
-    res.status(200).json({
-      message: "User created successfully",
+    // jwt:
+    const payload = {
+      user: user.id,
+    };
+
+    jwt.sign(
+      payload,
+      process.env.jwtSecret,
+      {
+        expiresIn: "1hr",
+      },
+      (error, token) => {
+        if (error) throw error;
+        res.status(200).json({
+          message: token,
+        });
+      }
+    );
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({
+      error: "Server error",
     });
+  }
+});
+
+/*
+ * @route:  /user/login
+ * @desc:   authenticate a user
+ * @access: public
+ */
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    let user = await User.findOne({
+      where: {
+        email: email,
+      },
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        error: "Incorrect credentials",
+      });
+    }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({
+        error: "Incorrect credentials",
+      });
+    }
+
+    const payload = {
+      user: user.id,
+    };
+
+    jwt.sign(
+      payload,
+      process.env.jwtSecret,
+      {
+        expiresIn: "1hr",
+      },
+      (error, token) => {
+        if (error) throw error;
+        res.status(200).json({
+          message: token,
+        });
+      }
+    );
   } catch (error) {
     console.error(error.message);
     res.status(500).json({
